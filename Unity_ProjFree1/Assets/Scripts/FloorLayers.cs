d692de1403d48e8c341c0ef877b9b4ce51323bf3
Chantal <c_voort1992@hotmail.com>
2019-05-29 16:21:19 +0200

    updated beta scene, new version of floor behaivor added new factoryblockout, factory prefab, new mockup materials

---
+++ Unity_ProjFree1/Assets/Scripts/FloorLayers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLayers : MonoBehaviour{
+	private Transform player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player").transform.root;
+	}
+
+	void Update() {
+		FindChildren();
+	}
+
+	void EnableSwap(Transform child) {
+		if(player.localPosition.y < child.localPosition.y) {
+			child.GetComponent<MeshRenderer>().enabled = false;
+		} else {
+			child.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+
+	void FindChildren() {
+		foreach(Transform child in transform) {
+			if(child.GetComponent<MeshRenderer>() != null) {
+				EnableSwap(child);
+			} else {
+				EnableSwap(child.GetChild(0));
+			}
+		}
+	}
+}
