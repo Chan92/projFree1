3b3fcc48b7e8329669200f40472b571b7b52cb24
Snorro1998 <tjaard.van.verseveld@gmail.com>
2019-06-19 14:30:40 +0200

    Several models added and textures applied.

---
+++ Unity_ProjFree1/Assets/DoorAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("Euj");
+        }/*
+        // Change the cube color to green.
+        MeshRenderer meshRend = GetComponent<MeshRenderer>();
+        meshRend.material.color = Color.green;
+        Debug.Log(other.name);*/
+    }
+}
